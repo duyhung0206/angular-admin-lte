f24aa9d3522ef0e77c9c0a33f6359e3c04e7084f
duyhung <hung02061994@gmail.com>
2019-11-12 08:37:44 +0700

    update portal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppheaderComponent } from './layout/appheader/appheader.component';
+import { AppmenuComponent } from './layout/appmenu/appmenu.component';
+import { AppfooterComponent } from './layout/appfooter/appfooter.component';
+import { AppsettingComponent } from './layout/appsetting/appsetting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppheaderComponent,
+    AppmenuComponent,
+    AppfooterComponent,
+    AppsettingComponent,
   ],
   imports: [
     BrowserModule,
